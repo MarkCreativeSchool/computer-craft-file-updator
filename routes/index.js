a866f6aa944165c6e45d3b6b54b04edb7079bfef
Yuki-Shibanuma <smatosan@gmail.com>
2019-12-21 08:11:39 +0900

    add module

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /*
+  const dir = "/Users/mark.7/Library/Application Support/minecraft/versions/1.12.2-forge1.12.2-14.23.5.2768/saves/New World/computer"
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
+    console.log(files);
+  });
+   */
   res.render('index', { title: 'Express' });
 });
 
