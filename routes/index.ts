fc0478849a941a89a1403d7cb9b0e64f079f5340
Yuki-Shibanuma <smatosan@gmail.com>
2019-12-30 23:12:45 +0900

    ts

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,10 @@
 import {NextFunction, Request, Response} from "express";
-
+import { getHomeDir } from "../utils/path";
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log(getHomeDir())
   res.send('respond with a resource');
 });
 
